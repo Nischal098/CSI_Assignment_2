4e54b6b9a9144aea72ee16e32219b2805a967b65
Nischal098 <nshar082@uottawa.ca>
2018-10-16 19:36:28 -0400

    update files with given info

---
+++ Block.java
@@ -0,0 +1,16 @@
+public class Block {
+ private int index; // the index of the block in the list
+ private java.sql.Timestamp timestamp; // time at which transaction
+ // has been processed
+ private Transaction transaction; // the transaction object
+ private String nonce; // random string (for proof of work)
+ private String previousHash; // previous hash (set to "" in first block)
+ //(in first block, set to string of zeroes of size of complexity "00000")
+ private String hash; // hash of the block (hash of string obtained
+//from previous variables via toString() method)
+//...
+ public String toString() {
+ return timestamp.toString() + ":" + transaction.toString()
+ + "." + nonce+ previousHash;
+}
+}
---
+++ Transaction.java
@@ -0,0 +1,9 @@
+public class Transaction {
+	private String sender;
+	private String receiver;
+	private int amount;
+	//…
+	public String toString() {
+		return sender + ":" + receiver + "=" + amount;
+	}
+}
