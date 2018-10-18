6ccbd41cdfeedc896d7fc7fbc0740cd9462d8e15
Nischal098 <nshar082@uottawa.ca>
2018-10-18 12:43:04 -0400

    2

--- BlockChain.java
+++ BlockChain.java
@@ -1,13 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
 public class BlockChain{
-//Repeat Set block nonce to <next random string of visible characters>
+	//Repeat Set block nonce to <next random string of visible characters>
 	 //Until ( SHA-1(block.toString()) starts with ‘00000’ in hexa )
 	
+	public BlockChain(ArrayList text){
+		
+	}
+	
 	public static void main(String[] args) {
+		//first prompt user for file name
+		String inputFile;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the File Name to read: ");
+		inputFile = scan.next();
+		
+		BlockChain returnRead = fromFile(inputFile);
 		
 	}
 	
 	public static BlockChain fromFile(String fileName) {
 		//add into array list
+		File file = new File(fileName);
+		
+		
 		return null;
 	}
 	
