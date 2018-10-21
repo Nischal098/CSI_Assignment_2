2c96a877608a8d31275f7f3b0a44890bebde9164
Nischal098 <nshar082@uottawa.ca>
2018-10-21 15:44:11 -0400

    reader

--- BlockChain.java
+++ BlockChain.java
@@ -5,29 +5,45 @@ import java.io.*;
 public class BlockChain{
 	//Repeat Set block nonce to <next random string of visible characters>
 	 //Until ( SHA-1(block.toString()) starts with ‘00000’ in hexa )
-	ArrayList blocks;
-	public BlockChain(ArrayList text){
-		blocks = text;
+	
+	ArrayList<Block> arrayList;
+	public BlockChain(ArrayList<Block> fileBlocks){
+		arrayList = fileBlocks;
 	}
 	
 	public static void main(String[] args) {
-		//first prompt user for file name
 		String inputFile;
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the File Name to read: ");
 		inputFile = scan.next();
 		
-		BlockChain returnRead = fromFile(inputFile);
-		
+		BlockChain returnRead = fromFile(inputFile + ".txt");
 	}
 	
 	public static BlockChain fromFile(String fileName) {
 		//add into array list
-		File file = new File("\\"+fileName+".txt");
-		BufferedReader br = new BufferedReader(new FileReader(file));
+		File file = new File(fileName);
+		BufferedReader reader;
+		ArrayList<String> textFileInputs = new ArrayList<String>();
 		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			
+			String info;
+			while ((info = reader.readLine()) != null) {
+				textFileInputs.add(info);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not Found");
+		} catch (IOException e) {
+			System.out.println("Error reading file");
+		}
 		
+		for(int i = 0; i<= textFileInputs.size()-1; i+= 7) {
+			//System.out.println(textFileInputs.get(i));
+			
+		}
 		return null;
 	}
 	
